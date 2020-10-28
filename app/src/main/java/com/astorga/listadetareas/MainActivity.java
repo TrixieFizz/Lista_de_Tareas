aaf158b239234ea95901994949d24e4a91b0d632
Gedeon <gedeon.astorga@gmail.com>
2020-10-28 13:28:39 -0300

    Añadio boton flotante que envia para añadir una tarea

--- app/src/main/java/com/astorga/listadetareas/MainActivity.java
+++ app/src/main/java/com/astorga/listadetareas/MainActivity.java
@@ -1,10 +1,17 @@
 package com.astorga.listadetareas;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +22,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         spinner_dias = findViewById(R.id.spinner_Dias);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intento = new Intent(MainActivity.this, AnadirTarea.class);
+                startActivity(intento);
+            }
+        });
 
         ArrayAdapter<CharSequence> adaptador = ArrayAdapter.createFromResource(MainActivity.this,R.array.dias_array,
                 android.R.layout.simple_spinner_item);
